5b00580fa3a39c701e7568ce2bcbbdda44c06dae
jhpoelen <jhpoelen@xs4all.nl>
2022-06-24 09:22:50 -0500

    add make script

---
+++ make.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# Tracks, builds, and converts Handbook of the Mammals of the World (HMW) portion of 
+# Plazi's Treatment Bank into HMW specific json, and csv files.
+#
+#
+
+set -x
+
+function track { 
+  preston track https://github.com/plazi/treatments-xml/archive/master.zip
+}
+
+function build {
+  time preston ls\
+| preston plazi-stream\
+| grep "Handbook of the Mammals of the World"\
+| gzip\
+> hmw.json.gz
+}
+
+function convert {
+  time cat hmw.json.gz\
+| gunzip\
+| jq -f schema.jq\
+| mlr --ijson --ocsv cat\
+| tee hmw.csv
+}
+
+preston version
+
+track
+build
+convert
